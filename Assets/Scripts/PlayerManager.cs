391ba4125d705678644a6b2be82fe74bc63f5776
Khalid Hasan <sawmaster22@gmail.com>
2024-01-08 22:28:07 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    Vector2 moveInput;
+
+    void Update()
+    {
+        
+    }
+}
