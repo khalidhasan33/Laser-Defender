e762afc78fb2ceeb9df7b71228723144c019c1a3
Khalid Hasan <sawmaster22@gmail.com>
2024-01-08 23:19:26 +0700

    add[movement] : playermanager and prefab

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,10 +7,50 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
 
+    float paddingLeft = 0.5f;
+    float paddingRight = 0.5f;
+    float paddingTop = 5f;
+    float paddingBottom = 1f;
+
     Vector2 moveInput;
+
+    Vector2 minBounds;
+    Vector3 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
 
     void Update()
     {
         
     }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0f, 0f));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1f, 1f));
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 delta = moveInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
 }
